0235fc2c18c84ec4da86e5a41c683ffb0ba471d1
RCSchmelzle <92551684+RCSchmelzle@users.noreply.github.com>
2024-04-08 07:22:40 -0400

    Gray World and Preset WB implemented

--- src/test.py
+++ src/test.py
@@ -4,15 +4,20 @@ import skimage.io
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Config
+img_path='data/baby.tiff'
+wb_method='gray' # White Balance method: gray, white, preset
 
 # 1.1.a Reconnaissance run
 black = 0
 white = 16383
+r_scale: 1.628906
+g_scale: 1.000000
+b_scale: 1.386719
 
 # 1.1.b Identify the Python Initials
 
 # Load in the tiff
-img_path='data/baby.tiff'
 img = skimage.io.imread(img_path, plugin='pil')
 
 # Display the width, height, bits per pixel
@@ -52,8 +57,60 @@ def bayer_id(img):
             pattern='rggb' # B is at [1,1]
     return pattern
 
+bayer_pattern = bayer_id(img)
 
 
+# 1.1.e White Balancing
+def white_balance(img, pattern, method="gray", rgb_weights=[1.0, 1.0, 1.0]):
+    if method=='gray':
 
+        TO DO: Check Greens only count once
+        if pattern=='gbrg' or pattern=='grbg':
+            g_mean=(img[0::2, 0::2].mean() + img[1::2, 1::2].mean())/2
+        else:
+            g_mean=(img[1::2, 0::2].mean() + img[0::2, 1::2].mean())/2
+        if pattern=='grbg':
+            r_mean=img[0::2, 1::2].mean()
+            b_mean=img[1::2, 0::2].mean()
+        elif pattern=='gbrg':
+            b_mean=img[0::2, 1::2].mean()
+            r_mean=img[1::2, 0::2].mean()
+        elif pattern=='bggr':
+            b_mean=img[0::2, 0::2].mean()
+            r_mean=img[1::2, 1::2].mean()
+        elif pattern=='rggb':
+            r_mean=img[0::2, 0::2].mean()
+            b_mean=img[1::2, 1::2].mean()
 
-bayer_id(img)
+        rgb_weights=[r_mean, r_mean/g_mean, r_mean/b_mean]
+    elif method=='white':
+        TO DO 
+        Calculate Weights
+
+    elif method=='presets': # camera presets
+        print('White balance using presets')
+        
+    # Set Greens
+    if pattern=='gbrg' or pattern=='grbg':
+        img[0::2, 0::2]*=rgb_weights[1]
+        img[1::2, 1::2]*=rgb_weights[1]
+    else:
+        img[1::2, 0::2]*=rgb_weights[1]
+        img[0::2, 1::2]*=rgb_weights[1]
+    
+    # Set Reds and Blues
+    if pattern=='grbg':
+        img[0::2, 1::2]*=rgb_weights[0]
+        img[1::2, 0::2]*=rgb_weights[2]
+    elif pattern=='gbrg':
+        img[0::2, 1::2]*=rgb_weights[2]
+        img[1::2, 0::2]*=rgb_weights[0]
+    elif pattern=='bggr':
+        img[0::2, 0::2]*=rgb_weights[2]
+        img[1::2, 1::2]*=rgb_weights[0]
+    elif pattern=='rggb':
+        img[0::2, 0::2]*=rgb_weights[0]
+        img[1::2, 1::2]*=rgb_weights[2]
+
+white_balance(img, bayer_pattern, method=wb_method, \
+               rgb_weights=[r_scale, g_scale, b_scale])
