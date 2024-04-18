4ba7f20eb6569b70c4ca52ca0bfff25ebb32ffa9
RCSchmelzle <92551684+RCSchmelzle@users.noreply.github.com>
2024-04-18 00:13:55 -0400

    Delete jasper_build directory

--- jasper_build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/rcsch/OneDrive/Desktop/ISP_homework/ISP_hw/jasper
-# Build directory: C:/Users/rcsch/OneDrive/Desktop/ISP_homework/ISP_hw/jasper_build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src/libjasper")
-subdirs("src/app")
-subdirs("doc")
